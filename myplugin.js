2d8dc14d9538aad69ad73046c6b378cc4badc5ff
marcosfreitas <marcosvsfreitas@gmail.com>
2015-12-28 11:52:07 -0300

    updated the section of call for methods or events
    Fix correct declaration of arguments array.

--- myplugin.js
+++ myplugin.js
@@ -44,34 +44,34 @@
 			}
 		};
 
-		
-		if (typeof opcoes !== 'undefined') {
+	   // call the methods or events
+	   var array_argumentos = Array();
 
-	    	try{
-				// Array.prototype.slice não funciona até o IE 8 e no chrome 14
+	    if (typeof opcoes !== 'undefined') {
+	    	try {
+			// Array.prototype.slice não funciona até o IE 8 e no chrome 14
 		        // http://stackoverflow.com/questions/7056925/how-does-array-prototype-slice-call-work
 		        // se não for um array ele vai separar a string em letras
 		        // para passar corretamente parâmetros para os métodos ou funçõesé preciso que opções seja um array,
 		        // ou um array-like (um objeto com índices numéricos e uma propriedade 'length')
 		        // repassa todos os elementos do array, mas depende de quantos a função está esperando receber
-		        var array_argumentos = Array.prototype.slice.call(opcoes, 0);
-
-		    } catch(err){
+		        array_argumentos = Array.prototype.slice.call(opcoes, 0);
+		} catch(err){
 
 		        // lento mas funciona no IE
-		        var array_argumentos = [],
-		            length = opcoes.length;
+		            array_argumentos = [];
+		        var length = opcoes.length;
 
 		        for(var o=0; o < length; o++){
 		            array_argumentos.push(opcoes[o]);
 		        }
 		    }
-		};
+	    }
 
-		// as funções dos objetos de métodos e eventos podem ser retornadas com metodo['nome_metodo'] por exemplo
-		if ( metodos[nome_funcao] ) {
-			// O método apply() chama uma função com um dado valor this e arguments providos como array (ou um objeto array-like ).
-			return metodos[nome_funcao].apply( this, array_argumentos);
+	    // as funções dos objetos de métodos e eventos podem ser retornadas com metodo['nome_metodo'] por exemplo
+	    if ( metodos[nome_funcao] ) {
+		// O método apply() chama uma função com um dado valor this e arguments providos como array (ou um objeto array-like ).
+		return metodos[nome_funcao].apply( this, array_argumentos);
 	    }
 	    else if ( eventos[nome_funcao] ) {
 	    	return eventos[nome_funcao].apply( this, array_argumentos);
@@ -81,14 +81,14 @@
 	    else if ( typeof opcoes === 'object' ) {
 	    	return metodos.init.apply( this, opcoes );
 	    }
-	    // chama o método init passando o objeto jquery seletor
+	    // chama o método init passando o objeto jquery seletor ou não
 	    else if ( typeof nome_funcao === 'undefined' && typeof opcoes === 'undefined' ) {
 	    	return metodos.init();
 	    	// somente se a função do plugin for do do tipo $.fn 
 	    	// return metodos.init(this);
 	    }
 	    else {
-	    	$.error( 'O Metodo ou Evento "' +  opcoes + '" nao existe em guia.min.js' );
+	    	$.error( 'O Metodo ou Evento "' +  nome_funcao + '" nao existe em log.min.js' );
 	    }
 
 	};
